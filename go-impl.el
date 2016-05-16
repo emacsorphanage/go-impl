00b614389e8ebdad5a873bcc8c8078df6cb1e5b2
Syohei YOSHIDA <syohex@gmail.com>
2016-05-16 17:25:37 +0900

    Implement completing interfaces

--- go-impl.el
+++ go-impl.el
@@ -26,8 +26,50 @@
 
 ;;; Code:
 
+(require 'go-mode)
+(require 'cl-lib)
+
+(defvar go-impl--interface-cache (make-hash-table :test #'equal))
 (defvar go-impl--receiver-history nil)
 (defvar go-impl--interface-history nil)
+
+(defun go-impl--collect-interface (package)
+  (with-temp-buffer
+    (unless (zerop (process-file "godoc" nil t nil "-src" package))
+      (error "Failed: 'godoc -src %s'" package))
+    (goto-char (point-min))
+    (cl-loop with re = "^type\\s-+\\(\\S-+\\)\\s-+interface"
+             with real-package = (if (string-match "[/-]\\(.+\\)\\'" package)
+                                     (match-string-no-properties 1 package)
+                                   package)
+             while (re-search-forward re nil t)
+             collect (concat real-package "." (match-string-no-properties 1)) into interfaces
+             finally return (progn
+                              (puthash package (cl-copy-list interfaces) go-impl--interface-cache)
+                              interfaces))))
+
+(defun go-impl--collect-interfaces (packages)
+  (cl-loop for package in packages
+           if (gethash package go-impl--interface-cache)
+           append it
+           else
+           append (go-impl--collect-interface package)))
+
+(defun go-impl--matched-packages (packages pattern)
+  (let ((package (if (string-match "\\." pattern)
+                     (substring pattern 0 (match-beginning 0))
+                   pattern)))
+   (cl-loop with regexp = (concat package "\\'")
+            for p in packages
+            when (string-match-p regexp p)
+            collect p)))
+
+(defun go-impl--completing-function (packages string predicate code)
+  (let* ((matched (go-impl--matched-packages packages string))
+         (candidates (go-impl--collect-interfaces matched)))
+    (if (not code)
+        (try-completion string candidates predicate)
+      (all-completions string candidates predicate))))
 
 (defun go-impl--execute (receiver interface)
   (with-temp-buffer
@@ -38,11 +80,14 @@
 ;;;###autoload
 (defun go-impl (receiver interface)
   (interactive
-   (list
-    (read-string "Receiver: " nil 'go-impl--receiver-history)
-    (read-string "Interface: " nil 'go-impl--interface-history)))
-   (let ((stubs (go-impl--execute receiver interface)))
-     (insert stubs)))
+   (let* ((packages (go-packages))
+          (comp-fn (lambda (input predicate code)
+                     (go-impl--completing-function packages input predicate code))))
+     (list
+      (read-string "Receiver: " nil 'go-impl--receiver-history)
+      (completing-read "Interface: " comp-fn nil nil nil 'go-impl--interface-history))))
+  (let ((stubs (go-impl--execute receiver interface)))
+    (insert stubs)))
 
 (provide 'go-impl)
 
